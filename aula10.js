e448455f8596d77842a789675392c512458ec333
FellipeMed <=>
2024-09-30 21:51:31 -0400

    fsjdifjdsiolfjsdlkjfsdlkj

--- aula10.js
+++ aula10.js
@@ -6,13 +6,13 @@ const port = 3000
 
 const server = http.createServer((req, res) => {
     const urlInfo = require('url').parse(req.url, true)
-    const name1 = urlInfo.query.name
+    const name1 = urlInfo.query.nameQualquer
     
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/html')
     
     if(!name1){
-        res.end('<form method="GET"><input type="text" name="Felipe" /><input type="submit" value="Send"></form>')
+        res.end('<form method="GET"><input type="text" name="nameQualquer" /><input type="submit" value="Send"></form>')
     } else{
         res.end(`<h1> Seja bem vindo ${name1}</h1>`)
     }
