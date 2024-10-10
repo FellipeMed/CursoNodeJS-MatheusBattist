286b6b51b7ccaaecb86ecded25c9015c67707281
FellipeMed <=>
2024-10-10 16:31:53 -0400

    fdjslifkjdslfjlsdkjfklsdjflskj

--- Express/index.js
+++ Express/index.js
@@ -5,6 +5,11 @@ const port = 3000
 
 const basePath = path.join(__dirname, 'templates')
 
+app.get('/users/:id', (req, res) =>{
+    const id = req.params.id
+    console.log(`Buscando usuario com o id ${id}`)
+    res.sendFile(`${basePath}/users.html`)
+})
 
 app.get('/', (req, res)=>{
     console.log("Vc está sendo requisitado!")
