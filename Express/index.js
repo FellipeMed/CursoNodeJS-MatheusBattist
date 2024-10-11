3734d82d624c51ee813d95483a22b61c92264764
Felipe Medeiros <49424311+FellipeMed@users.noreply.github.com>
2024-10-11 01:43:10 -0400

    fijsldfjsdlifj

--- Express/index.js
+++ Express/index.js
@@ -4,7 +4,21 @@ const app = express()
 const port = 3000
 
 const basePath = path.join(__dirname, 'templates')
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
+app.get('/users/create', (req, res)=>{
+    res.sendFile(`${basePath}/form.html`)
+})
+
+app.get('/users/save', (req, res) =>{
+    console.log(req.body)
+
+    const name = req.body.name
+    const age = req.body.age
+
+    console.log(`Seu nome é ${name} e sua idade é ${age}`)
+})
 
 app.get('/', (req, res)=>{
     console.log("Vc está sendo requisitado!")
