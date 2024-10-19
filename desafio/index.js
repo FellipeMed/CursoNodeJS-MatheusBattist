c7e13a17a833a0406797585819ac5c649073b5e3
FellipeMed <=>
2024-10-18 20:45:45 -0400

    slidfjsdlifjsldkjflskdjflskdjfldskjfkjfldskjflsdkj

--- desafio/index.js
+++ desafio/index.js
@@ -5,6 +5,19 @@ const port = 5000
 
 const basePath = path.join(__dirname, 'templates')
 
+const checkAuth = (req, res, next) =>{
+    req.authStatus = true
+
+    if(req.authStatus){
+        console.log("esta logado")
+        next()
+    }else{
+        console.log("nao esta logado")
+        next()
+    }
+}
+
+app.use(checkAuth)
 
 app.get('/', (req, res)=>{
     console.log("Vc está sendo requisitado!")
@@ -14,6 +27,11 @@ app.get('/', (req, res)=>{
 app.get('/login', (req, res)=>{
     console.log("Vc está sendo requisitado!")
     res.sendFile(`${basePath}/login.html`)
+})
+
+app.get('/dashboard', (req, res)=>{
+    console.log("Vc está sendo requisitado!")
+    res.sendFile(`${basePath}/dashboard.html`)
 })
 
 app.listen(port, ()=> {
